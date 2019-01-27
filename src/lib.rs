38d891ba56bb08a079476d62b16690f09dc9f630
Marcin Kolny <marcin.kolny@gmail.com>
2019-01-27 18:06:39 +0000

    Export CoreEventKlassId enum

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,6 @@
 pub mod registry;
 pub use crate::registry::EventKlassRegistry;
+pub use crate::registry::CoreEventKlassId;
 pub mod reader;
 pub use crate::reader::EventReader;
 pub use crate::reader::ReadEventError;
